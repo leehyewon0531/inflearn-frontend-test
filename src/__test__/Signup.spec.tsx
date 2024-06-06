0c296bf8cc3829b5f2be560ef8fe0d4524553065
leehyewon0531 <ifpffk@naver.com>
2024-06-06 16:29:49 +0900

    2-5 beforeEach를 활용 및 jest 성공 케이스 작성

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,7 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@testing-library/jest-dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { fireEvent, screen, render } from "@testing-library/react";
-import SignupPage from "pages/SignupPage";
+import SignupPage from "../pages/SignupPage";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 
 const queryClient = new QueryClient({
@@ -9,8 +10,7 @@ const queryClient = new QueryClient({
 });
 
 describe("회원가입 테스트", () => {
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메시지 표시된다", async () => {
-    // given - 회원가입 페이지가 그려짐
+  beforeEach(() => {
     const routes = [
       {
         path: "/signup",
@@ -27,6 +27,10 @@ describe("회원가입 테스트", () => {
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
+  });
+
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메시지 표시된다", async () => {
+    // given - 회원가입 페이지가 그려짐
 
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
     const passwordInput = screen.getByLabelText("비밀번호");
@@ -44,9 +48,21 @@ describe("회원가입 테스트", () => {
 
   test("이메일을 입력하고, 비밀번호와 비밀번호 확인값이 일치하면 회원가입 버튼이 활성화된다", () => {
     // given - 회원가입 페이지가 그려짐
+    const signupButton = screen.getByRole('button', { name: '회원가입' });
+    expect(signupButton).toBeDisabled();
 
     // when - 이메일 입력, 비밀번호, 비밀번호 확인 일치
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인");
+
+    fireEvent.change(emailInput, { target: { value: 'button-activated@email.com' } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmPasswordInput, { 
+      target: { value: "wrongPassword" } 
+    });
 
     // then - 회원가입 버튼 활성화
+    expect(signupButton).toBeEnabled();
   });
 });
