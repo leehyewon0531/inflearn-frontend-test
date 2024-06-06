07fb8ede01dbcc27085d6b2d4744f10542307415
leehyewon0531 <ifpffk@naver.com>
2024-06-06 17:03:56 +0900

    2-7 nock을 활용한 HTTP request mocking

--- src/__test__/Login.spec.tsx
+++ src/__test__/Login.spec.tsx
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 
-import { render, renderHook, waitFor } from "@testing-library/react";
+import * as nock from "nock";
+import { fireEvent, render, renderHook, screen, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LoginPage from "../pages/LoginPage";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
@@ -10,7 +11,19 @@ const queryClient = new QueryClient({
   defaultOptions: {},
 });
 
+const wrapper = ({ children }) => {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+};
+
 describe("로그인 테스트", () => {
+  beforeEach(() => {
+    jest.spyOn(console, "error").mockImplementation(() => { });
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
   test("로그인에 실패하면 에러메시지가 나타난다", async () => {
     // given - 로그인 화면이 그려짐
     const routes = [
@@ -31,13 +44,24 @@ describe("로그인 테스트", () => {
     );
 
     // when - 사용자가 로그인에 실패
-    const wrapper = ({ children }) => {
-      return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    };
+    nock("https://inflearn.byeongjinkang.com")
+      .post("/user/login", { username: "wrong@email.com", password: "wrongPassword" })
+      .reply(400, { msg: "NO_SUCH_USER" });
+
+    const emailInput = screen.getByLabelText("이메일");
+    const passwordInput = screen.getByLabelText("비밀번호");
+
+    fireEvent.change(emailInput, { target: { value: "wrong@email.com" } });
+    fireEvent.change(passwordInput, { target: { value: "wrongPassword" } });
+
+    const loginButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.click(loginButton);
 
     const { result } = renderHook(() => useLogin(), { wrapper });
 
     // then - 에러메시지가 나타남
     await waitFor(() => result.current.isError);
+    const errorMessage = await screen.findByTestId("error-message");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
