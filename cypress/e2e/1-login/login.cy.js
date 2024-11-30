dc41781a22274eb578bb9dc08700b987ba927fc1
leehyewon0531 <ifpffk@naver.com>
2024-11-30 16:35:40 +0900

    4-3 cypress에서 HTTP request mocking

--- cypress/e2e/1-login/login.cy.js
+++ cypress/e2e/1-login/login.cy.js
@@ -11,6 +11,17 @@ describe("로그인 화면", () => {
     cy.get("@emailInput").invoke("val").should("eq", "test@email.com");
     cy.get("@passwordInput").invoke("val").should("eq", "password");
 
+    // request가 서버에 들어가지 않도록 intercept
+    cy.intercept(
+      {
+        method: "POST",
+        url: "/user/login",
+      },
+      {
+        token: "AUTH_TOKEN",
+      }
+    ).as("login");
+
     cy.get("[data-cy=loginButton]").should("exist").click();
     // then - 로그인에 성공하고 메인화면으로 이동한다
     cy.url().should("include", "http://localhost:5173/");
